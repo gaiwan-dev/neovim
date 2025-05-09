0aeed89ddaa48a75818d7bc792e72977ac346c4c
pygaiwan <moise.medici@gmail.com>
2025-05-09 22:14:44 +0100

    Update to Harpoon V2

--- nvim/lua/pygaiwan/lazy/vim_utils/harpoon.lua
+++ nvim/lua/pygaiwan/lazy/vim_utils/harpoon.lua
@@ -1,29 +1,39 @@
 return {
 	"theprimeagen/harpoon",
-	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
+	branch = "harpoon2",
 
-		local wk = require("which-key")
-		wk.add({
-			{ "<leader>a", group = "Harpoon", icon = "" },
-			{ "<leader>aa", mark.add_file, desc = "Add to Harpoon", mode = "n" },
-			{ "<leader>ae", ui.toggle_quick_menu, desc = "Open Harpoon", mode = "n" },
-		})
+	config = function()
+		-- local mark = require("harpoon.mark")
+		-- local ui = require("harpoon.ui")
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Add file to Harpoon list" })
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Toggle Harpoon list" })
 		vim.keymap.set("n", "<leader>1", function()
-			ui.nav_file(1)
+			harpoon:list():select(1)
 		end, { desc = "Open file in buffer 1" })
 		vim.keymap.set("n", "<leader>2", function()
-			ui.nav_file(2)
+			harpoon:list():select(2)
 		end, { desc = "Open file in buffer 2" })
 		vim.keymap.set("n", "<leader>3", function()
-			ui.nav_file(3)
+			harpoon:list():select(3)
 		end, { desc = "Open file in buffer 3" })
 		vim.keymap.set("n", "<leader>4", function()
-			ui.nav_file(4)
+			harpoon:list():select(4)
 		end, { desc = "Open file in buffer 4" })
 		vim.keymap.set("n", "<leader>5", function()
-			ui.nav_file(5)
+			harpoon:list():select(5)
 		end, { desc = "Open file in buffer 5" })
+		vim.keymap.set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end)
 	end,
 }
