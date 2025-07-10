454709c2332cecd4e1d4a62e23535c907ec83798
gaiwan-dev <moise.medici@gmail.com>
2025-07-10 19:08:24 +0100

    chore: remove commented lines

--- nvim/lua/pygaiwan/vim_options.lua
+++ nvim/lua/pygaiwan/vim_options.lua
@@ -2,9 +2,6 @@
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
--- vim.opt.relativenumber = true
--- vim.o.statuscolumn = "%s %l %=%{v:relnum?v:relnum:v:lnum} "
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -30,6 +27,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "100"
-
--- might be needed for obsidan
-vim.opt.conceallevel = 1
